6b884829bb81c667544ff0db6cbd44fc02ef5d2c
gravstein <77721250+gravstein@users.noreply.github.com>
2026-01-29 23:41:52 +0600

    Delete politblogApp/views.py
    views delete

--- politblogApp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home_view(request):
-    return render(request, "content/home.html")
-
-def category_view(request):
-    return render(request, "content/article.html")
